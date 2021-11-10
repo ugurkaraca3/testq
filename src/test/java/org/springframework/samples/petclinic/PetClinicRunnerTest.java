ce9d0de281346fa06b23ee892157134cac40dcaa
z004dy2k <z004dy2k@EVT02099NB.mshome.net>
2021-11-10 20:35:04 +0300

    sss

--- src/test/java/org/springframework/samples/petclinic/PetClinicRunnerTest.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.samples.petclinic;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = { "src/test/features/petClinicCucumber" })
-public class PetClinicRunnerTest extends AbstractTestNGCucumberTests{
-
-}
